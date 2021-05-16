617e5531a410bf461aca408c8b8c102244a4dd15
Artur Garlej <artur.garlej@gmail.com>
2021-05-16 23:18:10 +0200

    Shift string by the given number

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,20 @@
-const testString = "To jest tekst destowy do sprawdzania mechaniki szyfru Cezara";
+const testString = "To jest tekst testowy do sprawdzania mechaniki szyfru Cezara śćżź";
 
 function printMassage(wherePrint, massage) { document.getElementById(wherePrint).innerText = massage };
+
+
+function cipher (string, shift, wherePrint) {
+    let charArray = string.split('');
+    console.log(charArray);
+    charArray = charArray.map(letter => letter.charCodeAt(''));
+    console.log(charArray);
+    charArray = charArray.map(ascii => ascii == 32 ? ascii : ascii + shift);
+    console.log(charArray);
+    charArray = charArray.map(ascii => String.fromCharCode(ascii));
+    console.log(charArray);
+    let massage = "";
+    massage = charArray.join("");
+    console.log(massage);
+    document.getElementById(wherePrint).innerText = massage
+
+}
