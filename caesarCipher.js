8a90c16325bb5c478b97d2e4c38c333bbac8c433
Artur Garlej <artur.garlej@gmail.com>
2021-05-18 23:14:44 +0200

    Add inputs and condition recognizing Latin alphabet

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,14 +1,17 @@
-const testString = "To jest tekst testowy do sprawdzania mechaniki szyfru Cezara śćżź";
-
-function printMassage(wherePrint, massage) { document.getElementById(wherePrint).innerText = massage };
-
+function getText()  { return document.getElementById("inputText").value };
+function getShift() { return parseInt(document.getElementById("inputShift").value) };
 
 function cipher (string, shift, wherePrint) {
+    const A = "A".charCodeAt('');
+    const Z = "Z".charCodeAt('');
+    const a = "a".charCodeAt('');
+    const z = "z".charCodeAt('');
+
     let charArray = string.split('');
     console.log(charArray);
     charArray = charArray.map(letter => letter.charCodeAt(''));
     console.log(charArray);
-    charArray = charArray.map(ascii => ascii == 32 ? ascii : ascii + shift);
+    charArray = charArray.map(ascii => ascii >= A && ascii <= Z  || ascii >= a && ascii <= z ? ascii + shift : ascii);
     console.log(charArray);
     charArray = charArray.map(ascii => String.fromCharCode(ascii));
     console.log(charArray);
@@ -17,4 +20,4 @@ function cipher (string, shift, wherePrint) {
     console.log(massage);
     document.getElementById(wherePrint).innerText = massage
 
-}
+};
