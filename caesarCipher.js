d8a9dc5ce6484faa5031241d8514d51ad538a9a4
Artur Garlej <artur.garlej@gmail.com>
2021-05-19 10:34:44 +0200

    Refactor to consts

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,23 +1,23 @@
 function getText()  { return document.getElementById("inputText").value };
 function getShift() { return parseInt(document.getElementById("inputShift").value) };
 
-function cipher (string, shift, wherePrint) {
+function cipherString (string, shift, wherePrint) {
     const A = "A".charCodeAt('');
     const Z = "Z".charCodeAt('');
     const a = "a".charCodeAt('');
     const z = "z".charCodeAt('');
+    function isLetterUpp(ascii) { return ascii >= A && ascii <= Z};
+    function isLetterLow(ascii) { return ascii >= a && ascii <= z};
+
 
     let charArray = string.split('');
-    console.log(charArray);
-    charArray = charArray.map(letter => letter.charCodeAt(''));
-    console.log(charArray);
-    charArray = charArray.map(ascii => ascii >= A && ascii <= Z  || ascii >= a && ascii <= z ? ascii + shift : ascii);
-    console.log(charArray);
-    charArray = charArray.map(ascii => String.fromCharCode(ascii));
-    console.log(charArray);
-    let massage = "";
-    massage = charArray.join("");
-    console.log(massage);
+    // console.log("Tablica liter: " + charArray);
+    const asciiArray = charArray.map(letter => letter.charCodeAt(''));
+    // console.log("Tablica ascii: " + asciiArray);
+    const codedAsciiArray = asciiArray.map(ascii => isLetterUpp(ascii) || isLetterLow(ascii) ? ascii + shift : ascii);
+    // console.log("Tablica zakodowanych ascii: " + codedAsciiArray);
+    const codedCharArray = codedAsciiArray.map(ascii => String.fromCharCode(ascii));
+    // console.log("Tablica zakodowanych liter: " + codedCharArray);
+    const massage = codedCharArray.join("");
     document.getElementById(wherePrint).innerText = massage
-
 };
