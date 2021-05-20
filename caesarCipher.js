6f273bf27f6e9f0e8e534c3b44718ed3fb1bab1c
Artur Garlej <artur.garlej@gmail.com>
2021-05-20 22:46:51 +0200

    Loop the alphabet

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,20 +1,32 @@
-function getText()  { return document.getElementById("inputText").value };
-function getShift() { return parseInt(document.getElementById("inputShift").value) };
+getText = () => document.getElementById("inputText").value;
+getShift = () => parseInt(document.getElementById("inputShift").value);
 
 function cipherString (string, shift, wherePrint) {
     const A = "A".charCodeAt('');
     const Z = "Z".charCodeAt('');
     const a = "a".charCodeAt('');
     const z = "z".charCodeAt('');
-    function isLetterUpp(ascii) { return ascii >= A && ascii <= Z};
-    function isLetterLow(ascii) { return ascii >= a && ascii <= z};
+    const realShift = shift % (Z - A + 1);
+    isLetterUpp = (ascii) => ascii >= A && ascii <= Z;
+    isLetterLow = (ascii) => ascii >= a && ascii <= z;
+ 
 
 
     let charArray = string.split('');
     // console.log("Tablica liter: " + charArray);
     const asciiArray = charArray.map(letter => letter.charCodeAt(''));
     // console.log("Tablica ascii: " + asciiArray);
-    const codedAsciiArray = asciiArray.map(ascii => isLetterUpp(ascii) || isLetterLow(ascii) ? ascii + shift : ascii);
+    const codedAsciiArray = asciiArray.map(ascii => 
+        isLetterLow(ascii) && isLetterLow(ascii + realShift) ? ascii + realShift :
+            isLetterLow(ascii) && (ascii + realShift) > z ? (a - 1) + (realShift - (z - ascii)) :
+                isLetterLow(ascii) && (ascii + realShift) < a ? (z + 1) + (realShift + (ascii - a)) :
+                    isLetterUpp(ascii) && isLetterUpp(ascii + realShift) ? ascii + realShift :
+                        isLetterUpp(ascii) && (ascii + realShift) > Z ? (A - 1) + (realShift - (Z - ascii)) :
+                            isLetterUpp(ascii) && (ascii + realShift) < A ? (Z + 1) + (realShift + (ascii - A)) :
+                                ascii        
+    );
+
+        
     // console.log("Tablica zakodowanych ascii: " + codedAsciiArray);
     const codedCharArray = codedAsciiArray.map(ascii => String.fromCharCode(ascii));
     // console.log("Tablica zakodowanych liter: " + codedCharArray);
