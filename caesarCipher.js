89516dc96deef83d349704798e9b5b795caaf95a
Artur Garlej <artur.garlej@gmail.com>
2021-05-26 12:58:11 +0200

    Calculate the percentage of letters in the string

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,7 +1,116 @@
 getText = () => document.getElementById("inputText").value;
 getShift = () => parseInt(document.getElementById("inputShift").value);
 
-function cipherString (string, shift, wherePrint) {
+const string ="Ala ma kota, a kot ma Alę";
+
+const statPl = {
+    a:	8.91,
+    ą:	0.99,
+    b:	1.47,
+    c:	3.96,
+    ć:	0.40,
+    d:	3.25,
+    e:	7.66,
+    ę:	1.11,
+    f:	0.30,
+    g:	1.42,
+    h:	1.08,
+    i:	8.21,
+    j:	2.28,
+    k:	3.51,
+    l:	2.10,
+    ł:	1.82,
+    m:	2.80,
+    n:	5.52,
+    ń:	0.20,
+    o:	7.75,
+    ó:	0.85,
+    p:	3.13,
+    q:	0.14,
+    r:	4.69,
+    s:	4.32,
+    ś:	0.66,
+    t:	3.98,
+    u:	2.50,
+    v:	0.04,
+    w:	4.65,
+    x:	0.02,
+    y:	3.76,
+    z:	5.64,
+    ź:	0.06,
+    ż:	0.83,
+};
+
+
+const alphabetPl = [
+    'a',
+    'ą',
+    'b',
+    'c',
+    'ć',
+    'd',
+    'e',
+    'ę',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'ł',
+    'm',
+    'n',
+    'ń',
+    'o',
+    'ó',
+    'p',
+    'q',
+    'r',
+    's',
+    'ś',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'ź',
+    'ż',
+];
+
+function countLetters (string, char)
+{
+    char = char.toUpperCase();
+    string = string.toUpperCase();
+    let charCounter = 0
+    for (position = 0; position < string.length; position ++)
+    {
+        string.charAt(position) == char ? charCounter += 1 : charCounter;
+    }
+    return charCounter;
+};
+
+function calcPercentLetters (string, stat)
+{
+    let statString = new Object();
+    Object.keys(stat).map(letter => statString[letter] = countLetters(string,letter));
+    let letterNumber = 0;
+    for (let letter in statString){
+        if (statString.hasOwnProperty(letter)){
+            letterNumber += parseFloat( statString[letter]);
+        };
+    };
+    for (let letter in statString){
+        if (statString.hasOwnProperty(letter)){
+            statString[letter] = statString[letter]*100/letterNumber;
+        };
+    };
+    return statString;
+};
+
+function cipher (string, shift, wherePrint) {
     const A = "A".charCodeAt('');
     const Z = "Z".charCodeAt('');
     const a = "a".charCodeAt('');
@@ -13,9 +122,7 @@ function cipherString (string, shift, wherePrint) {
 
 
     let charArray = string.split('');
-    // console.log("Tablica liter: " + charArray);
     const asciiArray = charArray.map(letter => letter.charCodeAt(''));
-    // console.log("Tablica ascii: " + asciiArray);
     const codedAsciiArray = asciiArray.map(ascii => 
         isLetterLow(ascii) && isLetterLow(ascii + realShift) ? ascii + realShift :
             isLetterLow(ascii) && (ascii + realShift) > z ? (a - 1) + (realShift - (z - ascii)) :
@@ -27,9 +134,8 @@ function cipherString (string, shift, wherePrint) {
     );
 
         
-    // console.log("Tablica zakodowanych ascii: " + codedAsciiArray);
     const codedCharArray = codedAsciiArray.map(ascii => String.fromCharCode(ascii));
-    // console.log("Tablica zakodowanych liter: " + codedCharArray);
     const massage = codedCharArray.join("");
     document.getElementById(wherePrint).innerText = massage
 };
+
