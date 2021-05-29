c37adae47101407c0e2961b1af6766ba2f1eba41
Artur Garlej <artur.garlej@gmail.com>
2021-05-29 17:51:30 +0200

    Add decode function for polish signs (ą,ę,ś,ć,ż)

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,5 +1,5 @@
-getText = () => document.getElementById("inputText").value;
-getShift = () => parseInt(document.getElementById("inputShift").value);
+const getText = () => document.getElementById("inputText").value;
+const getShift = () => parseInt(document.getElementById("inputShift").value);
 
 const string ="Ala ma kota, a kot ma Alę";
 
@@ -19,7 +19,7 @@ const statPl = {
     j:	2.28,
     k:	3.51,
     l:	2.10,
-    ł:	1.82,
+    ł:	1.82, 
     m:	2.80,
     n:	5.52,
     ń:	0.20,
@@ -110,16 +110,60 @@ function calcPercentLetters (string, stat)
     return statString;
 };
 
-function cipher (string, shift, wherePrint) {
+function findKey (string, stat, alphabet)
+{
+    let lowestDiff = Infinity;
+    let loopBest = null;
+    const statString = calcPercentLetters(string, stat);
+
+    for (let i = 0; i <  alphabet.length; i++)
+    {        
+        const tekstVsStatistic = alphabet.map(letter => Math.abs(
+            alphabet.indexOf(letter)+i < alphabet.length ? stat[alphabet[alphabet.indexOf(letter)+i]] - statString[letter] : stat[alphabet[-1+i-(alphabet.length-1-alphabet.indexOf(letter))]] - statString[letter]));
+        const difference = tekstVsStatistic.reduce((a, b) => a + b, 0);
+
+        if (lowestDiff > difference) {
+            lowestDiff = difference;
+            loopBest = i;
+            console.log(`Najmniejszą różnicę równą ${lowestDiff} znaleziono przy pętli ${loopBest}. Stary klucz wynosił: ${alphabet.length - loopBest}`);
+        };
+    };
+    return loopBest;
+    // document.getElementById(wherePrint).innerText = `Poszukiwany klucz: ${alphabet.length -loopBest}`
+};
+
+
+
+function cipherAlphabet (string, stat, alphabet, wherePrint) 
+{
+    const alphabetUpper = alphabet.map(letter => letter.toUpperCase());
+    const key = findKey (string, stat, alphabet);
+    const isLetterLow = (letter, key) => alphabet.includes(alphabet[alphabet.indexOf(letter) + key]);
+    const isLetterUpp = (letter, key) => alphabetUpper.includes(alphabetUpper[alphabetUpper.indexOf(letter) + key]);
+
+    const charArray = string.split('');
+    const codedCharArray = charArray.map(letter => 
+        isLetterLow(letter, 0) && isLetterLow(letter, key) ? alphabet[alphabet.indexOf(letter)+key] :
+            isLetterLow(letter, 0) && (alphabet.indexOf(letter) + key) >= alphabet.length ? alphabet[(- 1) + (key - ((alphabet.length -1) - alphabet.indexOf(letter)))] :
+                isLetterLow(letter, 0) && (alphabet.indexOf(letter) + key) < 0 ? alphabet[(alphabet.length) + (key + alphabet.indexOf(letter))] :
+                    isLetterUpp(letter, 0) && isLetterUpp(letter, key) ? alphabetUpper[alphabetUpper.indexOf(letter)+ key] :
+                        isLetterUpp(letter, 0) && (alphabetUpper.indexOf(letter) + key) >= alphabetUpper.length ? alphabetUpper[(- 1) + (key - ((alphabetUpper.length -1) - alphabetUpper.indexOf(letter)))] :
+                            isLetterUpp(letter, 0) && (alphabetUpper.indexOf(letter) + key) < 0 ? alphabetUpper[(alphabetUpper.length) + (key + alphabetUpper.indexOf(letter))] :
+                                letter
+    );
+    const massage = codedCharArray.join("");
+    document.getElementById(wherePrint).innerText = massage;
+};
+
+function cipherAsci (string, shift, wherePrint) 
+{
     const A = "A".charCodeAt('');
     const Z = "Z".charCodeAt('');
     const a = "a".charCodeAt('');
     const z = "z".charCodeAt('');
     const realShift = shift % (Z - A + 1);
-    isLetterUpp = (ascii) => ascii >= A && ascii <= Z;
-    isLetterLow = (ascii) => ascii >= a && ascii <= z;
- 
-
+    const isLetterUpp = (ascii) => ascii >= A && ascii <= Z;
+    const isLetterLow = (ascii) => ascii >= a && ascii <= z;
 
     let charArray = string.split('');
     const asciiArray = charArray.map(letter => letter.charCodeAt(''));
@@ -132,7 +176,6 @@ function cipher (string, shift, wherePrint) {
                             isLetterUpp(ascii) && (ascii + realShift) < A ? (Z + 1) + (realShift + (ascii - A)) :
                                 ascii        
     );
-
         
     const codedCharArray = codedAsciiArray.map(ascii => String.fromCharCode(ascii));
     const massage = codedCharArray.join("");
